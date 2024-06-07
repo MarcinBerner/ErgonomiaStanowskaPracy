94f0ed1e41980eb6931a71de3556ccb5200e9c8c
Marcin Berner <marcinberner@gmail.com>
2024-06-07 19:59:36 +0200

    Form setup

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -11,77 +11,99 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrl: './form.component.css',
 })
 export class FormComponent {
+  worker = new Worker();
 
-worker = new Worker();
+  constructor(
+    private workerService: WorkerService,
+    private _formBuilder: FormBuilder
+  ) {}
 
 
 
- constructor(private workerService: WorkerService, private _formBuilder: FormBuilder){}
+  personalData = this._formBuilder.group({
+    name: ['', Validators.required],
+    surname: ['', Validators.required],
+  });
+  firstQuestion = this._formBuilder.group({
+    seatHeight: ['', Validators.required],
+    seatHeightExtraFirst: [''],
+    seatHeightExtraSecond: [''],
+  });
+  secondQuestion = this._formBuilder.group({
+    seatDepth: ['', Validators.required],
+    seatDepthExtra: [''],
+  });
+  thirdQuestion = this._formBuilder.group({
+    chairTime: ['', Validators.required],
+  });
+  fourthQuestion = this._formBuilder.group({
+    armrests: ['', Validators.required],
+    armrestsExtraFirst: [''],
+    armrestsExtraSecond: [''],
+    armrestsExtraThird: [''],
 
- test():void{
- }
- saveData(): void{
-  if(this.personalData.valid){
-  this.getData();
-  const workerData = {
-    name: this.worker.name,
-    surname: this.worker.surname,
-    questions: this.worker.questions,
-  }
- this.workerService.writeWorkers(workerData);
-  }
- }
+  });
+  fifthQuestion = this._formBuilder.group({
+    chairBackrest: ['', Validators.required],
+    chairBackrestExtra: [''],
+  });
+  sixthQuestion = this._formBuilder.group({
+    screen: ['', Validators.required],
+    screenExtraFirst: [''],
+    screenExtraSecond: [''],
+    screenExtraThird: [''],
+    screenExtraFourth: [''],
+  });
+  seventhQuestion = this._formBuilder.group({
+    screenTime: ['', Validators.required],
+  });
+  eighthQuestion = this._formBuilder.group({
+    phone: ['', Validators.required],
+    phoneExtraFirst: [''],
+    phoneExtraSecond: [''],
+  });
+  ninethQuestion = this._formBuilder.group({
+    phoneTime: ['', Validators.required],
+  });
+  tenthQuestion = this._formBuilder.group({
+    mouse: ['', Validators.required],
+    mouseExtraFirst: [''],
+    mouseExtraSecond: [''],
+    mouseExtraThird: [''],
 
- personalData = this._formBuilder.group({
-  name: ['', Validators.required],
-  surname: ['', Validators.required],
+  });
+  eleventhQuestion = this._formBuilder.group({
+    mouseTime: ['', Validators.required],
+  });
+  twelvethQuestion = this._formBuilder.group({
+    keyboard: ['', Validators.required],
+    keyboardExtraFirst: [''],
+    keyboardExtraSecond: [''],
+    keyboardExtraThird: [''],
+    keyboardExtraFourth: [''],
+  });
+  thirteenthQuestion = this._formBuilder.group({
+    keyboardTime: ['', Validators.required],
+  });
 
-});
-firstQuestion = this._formBuilder.group({
-  secondCtrl: ['', Validators.required],
-});
-secondQuestion = this._formBuilder.group({
-  secondCtrl: ['', Validators.required],
-});
-thirdQuestion = this._formBuilder.group({
-  secondCtrl: ['', Validators.required],
-});
-fourthQuestion = this._formBuilder.group({
-  secondCtrl: ['', Validators.required],
-});
-fifthQuestion = this._formBuilder.group({
-  secondCtrl: ['', Validators.required],
-});
-sixthQuestion = this._formBuilder.group({
-  secondCtrl: ['', Validators.required],
-});
-seventhQuestion = this._formBuilder.group({
-  secondCtrl: ['', Validators.required],
-});
-eighthQuestion = this._formBuilder.group({
-  secondCtrl: ['', Validators.required],
-});
-ninethQuestion = this._formBuilder.group({
-  secondCtrl: ['', Validators.required],
-});
-tenthQuestion = this._formBuilder.group({
-  secondCtrl: ['', Validators.required],
-});
-eleventhQuestion = this._formBuilder.group({
-  secondCtrl: ['', Validators.required],
-});
-twelvethQuestion = this._formBuilder.group({
-  secondCtrl: ['', Validators.required],
-});
-thirteenthQuestion = this._formBuilder.group({
-  secondCtrl: ['', Validators.required],
-});
-
-isLinear = false;
-getData(){
+  isLinear = false;
+  getData(): void {
     this.worker.name = this.personalData.value.name!;
     this.worker.surname = this.personalData.value.surname!;
-
-}
-
+    console.log(this.firstQuestion.value);
+  }
+  saveData(): void {
+    if (this.personalData.valid) {
+      this.getData();
+      const workerData = {
+        name: this.worker.name,
+        surname: this.worker.surname,
+        questions: this.worker.questions,
+      };
+      this.workerService.writeWorkers(workerData);
+    }
+  }
+  doTheMath(): void {
+    
+  }
 }
