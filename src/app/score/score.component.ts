a8c37260876d5db8800ec8f7e53b4a5b3dfced83
Marcin Berner <marcinberner@gmail.com>
2024-08-05 23:04:34 +0200

    xlxs score export added

--- src/app/score/score.component.ts
+++ src/app/score/score.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { WorkerService } from '../core/services/worker.service';
+import * as XLSX from 'xlsx';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-score',
@@ -10,6 +12,7 @@ export class ScoreComponent {
   workers: any[] = [];
   currentPage: number = 1;
   itemsPerPage: number = 3;
+  selectedWorker: any;
   constructor(private workerService: WorkerService) {}
   async ngOnInit(): Promise<void> {
     try {
@@ -43,6 +46,34 @@ export class ScoreComponent {
 
   totalPages(): number {
     return Math.ceil(this.workers.length / this.itemsPerPage);
+  }
+   // Method to export data to Excel
+   exportToExcel(): void {
+    const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(this.workers);
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Workers');
+
+    // Generate Excel file
+    const wbout: ArrayBuffer = XLSX.write(wb, { bookType: 'xlsx', type: 'array' });
+
+    // Save file using file-saver
+    saveAs(new Blob([wbout], { type: 'application/octet-stream' }), 'workers.xlsx');
+  }
+
+  // Method to select worker for editing
+  editWorker(worker: any) {
+    this.selectedWorker = { ...worker }; // Make a copy of the worker for editing
+  }
+
+  // Method to update the worker's data
+  updateWorker() {
+    if (this.selectedWorker) {
+      const index = this.workers.findIndex(worker => worker.id === this.selectedWorker.id);
+      if (index !== -1) {
+        this.workers[index] = this.selectedWorker;
+      }
+      this.selectedWorker = null; // Clear the selection
+    }
   }
 }
 
